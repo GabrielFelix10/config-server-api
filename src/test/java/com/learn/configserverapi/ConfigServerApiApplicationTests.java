3f26cdc68040293564fa458119ece73b4bbcdcfa
gfaraujo@uolinc.com <gafelix10@gmail.com>
2020-04-16 21:27:56 -0300

    first commit

---
+++ src/test/java/com/learn/configserverapi/ConfigServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.configserverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
